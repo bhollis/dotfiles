52336fb3412c064f0c2b90a958e97759c7f8876d
Ben Hollis <ben@benhollis.net>
2017-04-23 17:01:01 -0700

    Fix gifenc settings (but use licecap instead)

--- bin/gifenc.sh
+++ bin/gifenc.sh
@@ -2,7 +2,8 @@
 
 palette="/tmp/palette.png"
 
-filters="fps=15,scale=1200:-1:flags=lanczos"
+filters="fps=10,scale=1200:-1:flags=lanczos"
 
 ffmpeg -v warning -i $1 -vf "$filters,palettegen" -y $palette
 ffmpeg -v warning -i $1 -i $palette -lavfi "$filters [x]; [x][1:v] paletteuse" -y $2
+#gifsicle --optimize=3 $2 > $2
